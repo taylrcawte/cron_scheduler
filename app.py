8db3e75f8c7f77c5b1fc956b803e1c3e46b9a29f
Taylr Cawte <taylrcawte@gmail.com>
2023-09-09 10:49:41 -0400

    simple flask app to schedule jobs

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-#imports 
-import sys 
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+from crontab import CronTab
 
+app = Flask(__name__)
 
-def main():
-    exit_code=0
-    sys.exit(exit_code)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__=="__main__": 
-    main() 
+@app.route('/schedule', methods=['POST'])
+def schedule_job():
+    job_command = request.form['command']
+    cron_schedule = request.form['schedule']
+
+    # Create a CronTab object
+    cron = CronTab(user='your_username')
+
+    # Create a new cron job
+    job = cron.new(command=job_command)
+
+    # Set the job schedule
+    job.setall(cron_schedule)
+
+    # Write the job to the user's crontab
+    job.set_comment("Scheduled Job")
+    cron.write()
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
